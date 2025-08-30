74f9fb49b21eb56075783520552b913a3512846e
rubberduckies12 <rowetommy699@gmail.com>
2025-08-31 00:52:15 +0100

    fav2.0

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/RCKTS.png" />
+        <link rel="icon" href="/RCKTSFAV.png" />
       </Head>
       <body className="antialiased">
         <Main />
